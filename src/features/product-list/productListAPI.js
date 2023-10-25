40f0db0de51088e654d53afb177f644da93b2c0c
Kunalhume <kunal.hume33@gmail.com>
2023-10-26 01:33:18 +0530

    Added Routing (Login / SignUp Page) Done

--- src/features/product-list/productListAPI.js
+++ src/features/product-list/productListAPI.js
@@ -2,8 +2,8 @@
 export function fetchCount(amount = 1) {
   return new Promise(async (resolve) =>{
     // setTimeout(() => resolve({ data: amount }), 500)
-    const response = await fetch('http://localhost:8080')  //Fetching data from API
-    const result = await response.json()
+    // const response = await fetch('http://localhost:8080')  //Fetching data from API
+    // const result = await response.json()
     // resolve({data})  
   }
   );
