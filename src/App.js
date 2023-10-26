018d500c4bc971806c75f6f3d1c8d0c50fb40467
Kunalhume <kunal.hume33@gmail.com>
2023-10-26 14:55:53 +0530

    Added CheckoutPage (Remaining)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import { BrowserRouter ,Route, Routes  } from 'react-router-dom';
 import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/signup" element={<SignupPage/>} />
         <Route path="/cart" element={<CartPage/>} />
+        <Route path="/checkout" element={<Checkout/>} />
       </Routes>
     </BrowserRouter>
   );
